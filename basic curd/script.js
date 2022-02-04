954185174de193176100808bc2a83fe93737ef9b
shahjash1212 <shahjash1212@gmail.com>
2022-02-04 16:21:47 +0530

    localstorage

--- basic curd/script.js
+++ basic curd/script.js
@@ -1,9 +1,37 @@
+let id='1';
+let myArr=JSON.parse(localStorage.getItem("formData")) || [];
 
+// var getlocalStorageData = localStorage.getItem("formData");
+console.log(myArr);
+
+for(var i=0;i<myArr.length;i++)
+{
+    id++;
+    var obj= myArr[i];
+    var table = document.getElementById("List").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = obj["inname"];
+    var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = obj["insalary"];
+    var cell3 = newRow.insertCell(2);
+        cell3.innerHTML = obj["inno"];
+    var cell5 = newRow.insertCell(3);
+        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+
+}
+
+
+  
 var selectedRow = null;
 function onFormSubmit(e){
     event.preventDefault();
     var formData = readFormData();
 
+    myArr.push(formData);
+    console.log(JSON.stringify(myArr));
+    localStorage.setItem('formData',JSON.stringify(myArr));
+    
 
     if(selectedRow === null){
         insertNewRecord(formData);
@@ -20,18 +48,16 @@ function onFormSubmit(e){
 function readFormData(){
     var formData = {};
     formData["inname"] = document.getElementById("inname").value;
-    if(formData["inname"] == null){
-        document.getElementById('inname').innerHTML="** Enter The Name"
-    }
     formData["insalary"] = document.getElementById("insalary").value;
     formData["inno"] = document.getElementById("inno").value;
-    localStorage.setItem('local',JSON.stringify(formData));
+
+    // localStorage.setItem('local',JSON.stringify(formData));
     return formData;
 }
 
 //Insert the data
 function insertNewRecord(data){
-    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("List").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     var cell1 = newRow.insertCell(0);
         cell1.innerHTML = data.inname;
@@ -40,7 +66,7 @@ function insertNewRecord(data){
     var cell3 = newRow.insertCell(2);
         cell3.innerHTML = data.inno;
     var cell5 = newRow.insertCell(3);
-        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button id onClick='onDelete(this.id)'>Delete</button>`
 }
 
 //Edit the data
@@ -61,12 +87,14 @@ function updateRecord(formData){
 }
 
 //Delete the data
-function onDelete(td){
+function onDelete(id){
     if(confirm('Do you want to delete this record?')){
         row = td.parentElement.parentElement;
         document.getElementById('storeList').deleteRow(row.rowIndex);
     }
-    resetForm();
+    myArr.slice(id,1);
+    localStorage.setItem('formData',JSON.stringify(myArr));
+    location.reload();
 }
 
 //Reset the data
